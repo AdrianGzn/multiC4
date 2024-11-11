4940b067404f2bb051bc5afe77c6c07afd586105
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2024-11-11 13:43:05 -0600

    details

--- src/app/welcome/welcome-patient/welcome-patient.component.ts
+++ src/app/welcome/welcome-patient/welcome-patient.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-welcome-patient',
   templateUrl: './welcome-patient.component.html',
   styleUrls: ['./welcome-patient.component.css']
 })
 export class WelcomePatientComponent {
+  constructor(private router: Router) { }
 
+  establishments(): void {
+    this.router.navigate(['/establishment/search']);
+  }
 }
