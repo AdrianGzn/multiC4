879e055d9a6a875d3bb3c5bf5dd055cbbdfbcf14
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2024-11-10 23:47:58 -0600

    cambios

--- src/app/user-log/sign-patient/sign-patient.component.ts
+++ src/app/user-log/sign-patient/sign-patient.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-patient',
   templateUrl: './sign-patient.component.html',
-  styleUrl: './sign-patient.component.css'
+  styleUrls: ['./sign-patient.component.css']
 })
 export class SignPatientComponent {
-  constructor(private router:Router){
+  signInForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.signInForm = this.fb.group({
+      usernameGroup: this.fb.group({
+        username: ['', Validators.required]
+      }),
+      passwordGroup: this.fb.group({
+        password: ['', Validators.required]
+      }),
+      confirmPasswordGroup: this.fb.group({
+        confirmPassword: ['', Validators.required]
+      })
+    });
   }
-  signEmployee(): void {
-    this.router.navigate(['/sign-employee']);
+
+  onSubmit(): void {
+    if (this.signInForm.valid) {
+      console.log('Formulario válido:', this.signInForm.value);
+    } else {
+      console.log('Formulario no válido');
+    }
   }
+
   signRegresar(): void {
-    this.router.navigate(['/sign-regresar']);
+    this.router.navigate(['/login']); 
   }
 
-  
+  signEmployee(): void {
+    this.router.navigate(['/sign-employee']); 
+  }
 }
