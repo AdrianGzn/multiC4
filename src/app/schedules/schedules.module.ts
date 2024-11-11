ad3b10e38ec3aeaaf2a9f2ab6964019d60d244a4
AdrianGzn <233318@ids.upchiapas.edu.mx>
2024-11-11 15:09:29 -0600

    formulario de horarios

--- src/app/schedules/schedules.module.ts
+++ src/app/schedules/schedules.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MyScheduleComponent } from './my-schedule/my-schedule.component';
 import { SchedulesRoutingModule } from './schedules-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ScheduleEmployeeComponent } from './schedule-employee/schedule-employee.component';
 
 @NgModule({
   declarations: [
-    MyScheduleComponent
+    ScheduleEmployeeComponent
   ],
   imports: [
     CommonModule,
