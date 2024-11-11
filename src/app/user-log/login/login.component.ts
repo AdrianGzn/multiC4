b75f555be830f4836a3cc9bc4ddddf211112fafb
AdrianGzn <233318@ids.upchiapas.edu.mx>
2024-11-11 09:49:08 -0600

    beta 3 de servicios

--- src/app/user-log/login/login.component.ts
+++ src/app/user-log/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GeneralServices } from '../../shared/services/general-services.service';
+import { log } from 'console';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +12,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder) {
+  constructor(private router: Router, private fb: FormBuilder, private generalServices: GeneralServices) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
-  onSubmit() {
+  onSubmit() {   
+    const loginData = {
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    };
+
     if (this.loginForm.valid) {
-      // Manejar el envío del formulario aquí
-      console.log('Formulario válido:', this.loginForm.value);
+      
+      this.generalServices.loginEmployee(loginData).subscribe({
+        next: (item: any) => {
+          console.log(item);
+        },
+        error: (err) => {
+          console.log("Error fetching data for currency : " + err);
+        }      
+      });
+
     } else {
       console.log('Formulario no válido');
     }
