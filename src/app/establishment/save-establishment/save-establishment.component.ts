fc5d46c05a00861a2d2e494091843b4f522ca02e
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2024-11-12 12:06:53 -0600

    cambios

--- src/app/establishment/save-establishment/save-establishment.component.ts
+++ src/app/establishment/save-establishment/save-establishment.component.ts
@@ -5,40 +5,46 @@ import { GeneralServices } from '../../shared/services/general-services.service'
 @Component({
   selector: 'app-save-establishment',
   templateUrl: './save-establishment.component.html',
-  styleUrl: './save-establishment.component.css'
+  styleUrls: ['./save-establishment.component.css']
 })
 export class SaveEstablishmentComponent {
-  formData: FormGroup;
-  formUbication: FormGroup;
-  formServices: FormGroup;
-  formDoctors: FormGroup;
+  form: FormGroup;
 
   constructor(private generalServices: GeneralServices) {
-    this.formData = new FormGroup({
-      nombre: new FormControl(null, [Validators.required]),
-      descripcion: new FormControl(null, [Validators.required]),
-    });
-
-    this.formUbication = new FormGroup({
-      calle: new FormControl(null, [Validators.required]),
-      colonia: new FormControl(null, [Validators.required]),
-      numero: new FormControl(null),
-      latitud: new FormControl(null),
-      longitud: new FormControl(null),
-      thisUbication: new FormControl(null),
-    });
-
-    this.formServices = new FormGroup({
-      servicio: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
-      costo: new FormControl(null, [Validators.required, Validators.min(10), Validators.max(250)]),
-    });
-
-    this.formDoctors = new FormGroup({
-      doctor: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
-      servicio: new FormControl(null, [Validators.required, Validators.min(10), Validators.max(250)]),
+    this.form = new FormGroup({
+      formData: new FormGroup({
+        nombre: new FormControl(null, [Validators.required]),
+        descripcion: new FormControl(null, [Validators.required]),
+        tipo: new FormControl(null, [Validators.required]),
+      }),
+      formUbication: new FormGroup({
+        calle: new FormControl(null, [Validators.required]),
+        colonia: new FormControl(null, [Validators.required]),
+        numero: new FormControl(null),
+        latitud: new FormControl(null),
+        longitud: new FormControl(null),
+        thisUbication: new FormControl(false),
+      }),
+      formServices: new FormGroup({
+        servicio: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
+        costo: new FormControl(null, [Validators.required, Validators.min(10), Validators.max(250)]),
+      }),
+      formDoctors: new FormGroup({
+        doctor: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
+        servicio: new FormControl(null, [Validators.required, Validators.min(10), Validators.max(250)]),
+      })
     });
   }
 
-  //lógica para verificar checkbox: event.target.checked
-
+  // Método onSubmit que envía el formulario al servicio
+  onSubmit() {
+    if (this.form.valid) {
+      const establishmentData = {
+        ...this.form.value.formData,
+        ...this.form.value.formUbication,
+        services: this.form.value.formServices,
+        doctors: this.form.value.formDoctors,
+      };
+    }
+}
 }
