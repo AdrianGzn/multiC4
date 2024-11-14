f4787e7f0e2579fc4e3b5b82122db4dd021929f0
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2024-11-14 13:34:21 -0600

    see cambios

--- src/app/appointments/see/see.component.ts
+++ src/app/appointments/see/see.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './see.component.css'
 })
 export class SeeComponent {
+  isOpen: boolean = false;
+  selectedOption: string = 'Atendidos';  
+  options: string[] = ['Atendidos', 'No Atendidos', 'En Espera', 'Cancelados'];
 
+  toggleSelect() {
+    this.isOpen = !this.isOpen;  
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;  
+    this.isOpen = false;  
+  }
 }
