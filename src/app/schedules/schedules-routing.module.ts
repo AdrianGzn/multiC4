247699713def6570c0eb8a98bfe944d637b42705
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2024-11-18 11:30:28 -0600

    cambios

--- src/app/schedules/schedules-routing.module.ts
+++ src/app/schedules/schedules-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ScheduleEmployeeComponent } from './schedule-employee/schedule-employee.component';
-
+import { authGuard } from '../welcome/guards/auth.guard';
 const routes: Routes = [
-    { path: 'scheduleEmployee', component: ScheduleEmployeeComponent },
+    { path: 'scheduleEmployee', component: ScheduleEmployeeComponent,canActivate: [authGuard]  },
   ];
   
   @NgModule({
