be83f5739221ec8e540a7cc5ebf009aa8492370c
AdrianGzn <233318@ids.upchiapas.edu.mx>
2024-11-22 12:19:05 -0600

    beta 3 de schedule doctor

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
   private baseUrl: string = 'http://3.227.141.174:8000';  
 
   private user: User = {
-    id_usuario: 0,
+    id_usuario: 1,
     id_rol: 0,
     nombre: '',
     id_establecimiento: 0,
