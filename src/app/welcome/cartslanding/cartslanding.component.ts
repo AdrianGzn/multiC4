941faa4a46e082bafbe80ea2b86fbd5c43e7ca6a
yuzuokogi18 <233351@ids.upchiapas.edu.mx>
2024-11-26 18:32:12 -0600

    landing

--- src/app/welcome/cartslanding/cartslanding.component.ts
+++ src/app/welcome/cartslanding/cartslanding.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { GeneralServices } from '../../shared/services/general-services.service';
 
 @Component({
@@ -11,31 +12,51 @@ export class CartslandingComponent implements OnInit {
   userFinal: any = {};
   currentSlide: number = 0;
 
-  isModalOpen: boolean = false; 
+  isModalOpen: boolean = false;
   selectedCampaign: any = null;
 
-  constructor(private generalService: GeneralServices) {}
+  servicios = [
+    { nombre: 'Odontología', img: '/assets/odontologia.jpeg', descripcion: 'Descripción de Odontología' },
+    { nombre: 'Nutrición', img: '/assets/nutrision.jpeg', descripcion: 'Descripción de Nutrición' },
+    { nombre: 'Ultrasonido', img: '/assets/ultrasonido.jpeg', descripcion: 'Descripción de Ultrasonido' },
+    { nombre: 'Podología', img: '/assets/podologia.jpeg', descripcion: 'Descripción de Podología' },
+    { nombre: 'Pediatría', img: '/assets/pediatria.jpeg', descripcion: 'Descripción de Pediatría' },
+    { nombre: 'Ginecología', img: '/assets/ginecologia.jpeg', descripcion: 'Descripción de Ginecología' },
+    { nombre: 'Dermatología', img: '/assets/dermatologia.jpeg', descripcion: 'Descripción de Dermatología' },
+    { nombre: 'Cardiología', img: '/assets/cardiologia.jpeg', descripcion: 'Descripción de Cardiología' },
+    { nombre: 'Fisiatría', img: '/assets/fisiatria.jpeg', descripcion: 'Descripción de Fisiatría' },
+  ];
+
+  serviciosIndex = 0;
+  visibleCards = 3;
+  modalVisible = false;
+  servicioSeleccionado: any = null;
+
+  windowWidth: number = window.innerWidth;
+
+  constructor(private generalService: GeneralServices, private router: Router) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowWidth = event.target.innerWidth;
+  }
 
   ngOnInit(): void {
-    let currentUser = localStorage.getItem('userData');
+    const currentUser = localStorage.getItem('userData');
     if (currentUser) {
       this.userFinal = JSON.parse(currentUser);
-      console.log(this.userFinal);
     }
     this.fetchCampaigns();
     this.startCarousel();
   }
 
   fetchCampaigns() {
-    const name = this.userFinal?.nombre || 'defaultName';
     this.generalService.getCampignsByNameWithOut().subscribe(
       (data: any) => {
-        console.log(data)
-        console.log('Datos de las campañas:', data);
         this.slides = data.map((campaign: any) => ({
           image: campaign.image || 'defaultImage.jpg',
-          title: campaign.campaign.nombre || 'Sin título', 
-          description: campaign.campaign.descripción || 'Sin descripción',
+          title: campaign.campaign.nombre || 'Sin título',
+          description: campaign.campaign.descripcion || 'Sin descripción',
         }));
       },
       (error) => {
@@ -49,20 +70,43 @@ export class CartslandingComponent implements OnInit {
   }
 
   startCarousel() {
-    if (this.slides.length > 0) {
-      setInterval(() => {
-        this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-      }, 3000);
-    }
+    setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }, 3000);
   }
 
   moreInfo(slide: any) {
-    this.selectedCampaign = slide; 
-    this.isModalOpen = true; 
+    this.selectedCampaign = slide;
+    this.isModalOpen = true;
   }
 
   closeModal() {
-    this.isModalOpen = false; 
-    this.selectedCampaign = null; 
+    this.isModalOpen = false;
+    this.selectedCampaign = null;
+  }
+
+  get visibleServicios() {
+    return this.servicios.slice(this.serviciosIndex, this.serviciosIndex + this.visibleCards);
+  }
+
+  prev() {
+    if (this.serviciosIndex > 0) {
+      this.serviciosIndex--;
+    }
+  }
+
+  next() {
+    if (this.serviciosIndex < this.servicios.length - this.visibleCards) {
+      this.serviciosIndex++;
+    }
+  }
+
+  abrirModal(servicio: any) {
+    this.servicioSeleccionado = servicio;
+    this.modalVisible = true;
+  }
+
+  cerrarModal() {
+    this.modalVisible = false;
   }
 }
